88defefbb5db374f413e30973b7f88c54987430d
ajinkya <ajinkyagawali12@gmail.com>
2023-10-11 22:45:30 +0530

    number spiral

---
+++ number_spiral.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int tt;
+	cin >> tt;
+
+	while(tt--){
+		int row, column;
+		std::vector<std::vector<int> arr;
+
+		cin >> row >> column;
+
+		int maxIter = max(row,column) * max(row,column);
+
+		for (int i = 0; i < maxIter; ++i)
+		{
+			
+		}
+
+	}
+	return 0;
+}
